150c86fb1537e9bd01eb1037b32eeaab8de8277b
learning-dev <ayazm.140895@gmail.com>
2019-11-27 10:13:31 +0530

    add code to populate db

--- populateDb.js
+++ populateDb.js
@@ -2,9 +2,6 @@ const mysql = require('mysql');
 const movieData = require('./movies.json');
 
 
-console.log(movieData.length);
-console.log(typeof movieData);
-
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'ayaz',
@@ -14,21 +11,39 @@ const connection = mysql.createConnection({
 
 let movieArray = [];
 const fieldsMovie = Object.keys(movieData[0]);
+let directorArray = [];
+let directorCount = 1;
+
+
+const insertQueryMovie = 'INSERT INTO Movie (movie_rank, Title, description, Runtime, genre, rating, metascore, '
+                        + 'votes, gross_earning_mil, director_id, actor, years) VALUES ?';
+
+const insertQueryDirector = 'INSERT INTO Director (director_id, director_name) VALUES ?';
 
 
 movieData.forEach((movie) => {
   let singleMovie = [];
 
   fieldsMovie.forEach((field) => {
-    singleMovie.push(movie[field]);
+    if (field === 'Director') {
+      let director = movie[field];
+      let directorId;
+      if (directorCount.toString().length === 1) {
+        directorId = `D0${directorCount.toString()}`;
+      } else {
+        directorId = `D${directorCount.toString()}`;
+      }
+      singleMovie.push(directorId);
+      if (directorArray.indexOf([directorId, director]) === -1) {
+        directorArray.push([directorId, director]);
+        directorCount += 1;
+      }
+    } else {
+      singleMovie.push(movie[field]);
+    }
   });
-
   movieArray.push(singleMovie);
 });
-
-
-const insertQuery = 'INSERT INTO Movie (movie_rank, Title, description, Runtime, genre, rating, metascore, votes, '
- + 'gross_earning_mil, director, actor, years) VALUES ?';
 
 connection.connect((err) => {
   if (err) {
@@ -36,11 +51,16 @@ connection.connect((err) => {
   } else {
     console.log('Connected!');
 
-    connection.query(insertQuery, [movieArray], (err, result, fields) => {
+    connection.query(insertQueryDirector, [directorArray], (err, result, fields) => {
       if (err) throw err;
       console.log(result);
     });
+
+    connection.query(insertQueryMovie, [movieArray], (err, result, fields) => {
+      if (err) throw err;
+      console.log(result);
+    });
+    connection.end();
   }
 });
 
-connection.end();
