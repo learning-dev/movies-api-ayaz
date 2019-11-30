e78e14a7123b132d825c6014ae8d817ec11b83fb
learning-dev <ayazm.140895@gmail.com>
2019-11-30 21:28:34 +0530

    partial refactor code

--- director.js
+++ director.js
@@ -1,60 +1,28 @@
-function getAllDirector() {
-  return new Promise((resolve, reject) => {
-    const getAllquery = 'SELECT * FROM Director';
-    connection.query(getAllquery, (err, result) => {
-      if (err) reject(err);
+const { queryDatabase } = require('./connectToDb');
 
-      const directorList = [];
-      for (let i = 0; i < result.length; i += 1) {
-        const singleEntry = {
-          id: result[i].id,
-          director_name: result[i].director_name,
-        };
-        directorList.push(singleEntry);
-      }
-      const directorListJson = { data: directorList };
-      return resolve(directorListJson);
-    });
-  });
+function getAllDirector() {
+  const getAllDirectorsQuery = 'SELECT * FROM Director';
+  return queryDatabase(getAllDirectorsQuery);
 }
 
 
 function getDirectorByID(id) {
-  const sqlQuery = `SELECT * FROM Director WHERE director_id = '${id}'`;
-  return new Promise((resolve, reject) => {
-    connection.query(sqlQuery, (err, result) => {
-      if (err) reject(err);
-      const singleDirector = {
-        director_id: result[0].id,
-        director_name: result[0].director_name,
-      };
-      const directorData = { data: singleDirector };
-      console.log(directorData);
-      return resolve(directorData);
-    });
-  });
+  const directorByIdQuery = `SELECT * FROM Director WHERE director_id = '${id}'`;
+  return queryDatabase(directorByIdQuery);
 }
 
 function addDirector(data) {
-  const sqlQuery = `INSERT INTO Director (id, director_name) VALUES ('${data.id}', '${data.director_name}');`;
+  const addDirectorQuery = `INSERT INTO Director (id, director_name) VALUES ('${data.director_name}');`;
 
-  return new Promise((resolve, reject) => {
-    connection.query(sqlQuery, (err, result) => {
-      if (err) reject(err);
-      console.log(result);
-      return resolve({ data: { message: `Director ${data.director_name} has been inserted in the database.` } });
-    });
-  });
+  return queryDatabase(addDirectorQuery);
+  // return resolve({ data: { message: `Director ${data.director_name} has been inserted in the database.` } });
 }
 
 function deleteDirector(id) {
-  const sqlQuery = `DELETE FROM Director WHERE id = '${id}';`;
-  return new Promise((resolve, reject) => {
-    connection.query(sqlQuery, (err, result) => {
-      if (err) return reject(err);
-      console.log(result);
-      console.log('Row deleted successfully!');
-      return resolve({ data: { message: `Director with id ${id} is deleted successfully.` } });
+  const deleteDirectorQuery = `DELETE FROM Director WHERE id = '${id}';`;
+  return queryDatabase(deleteDirectorQuery);
+    
+      //return resolve({ data: { message: `Director with id ${id} is deleted successfully.` } });
     });
   });
 }
