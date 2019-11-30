de54cbdf082e793803e63094dbb2ce7104091391
learning-dev <ayazm.140895@gmail.com>
2019-11-30 11:34:17 +0530

    make 'movie id' and 'director id' auto increment in table

--- populateDb.js
+++ populateDb.js
@@ -13,34 +13,25 @@ const movieArray = [];
 const fieldsMovie = Object.keys(movieData[0]);
 
 
-const insertQueryMovie = 'INSERT INTO Movie (movie_rank, title, description, runtime, genre, rating, metascore, '
+const createMovieQuery = 'CREATE TABLE Movie(id  INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(200), description VARCHAR(300),' +
+                            'runtime INT, genre varchar(100), rating FLOAT, metascore varchar(10), votes INT, ' + 
+                            'gross_earning_mil varchar(20), director_id INT, FOREIGN KEY (`director_id`) REFERENCES `Director`(`id`),' +
+                             'actor VARCHAR(200), year YEAR(4));';
+
+const createDirectorQuery = 'CREATE TABLE Director(id INT PRIMARY KEY AUTO_INCREMENT, director_name varchar(200));';
+
+
+const insertQueryMovie = 'INSERT INTO Movie (id, title, description, runtime, genre, rating, metascore, '
                         + 'votes, gross_earning_mil, director_id, actor, year) VALUES ?';
 
-const insertQueryDirector = 'INSERT INTO Director (director_name, director_id) VALUES ?';
+const insertQueryDirector = 'INSERT INTO Director (director_name) VALUES ?';
 
 const listOfDirector = [];
+
 movieData.forEach((movie) => {
   if (listOfDirector.indexOf(movie.Director) === -1) {
     listOfDirector.push(movie.Director);
   }
-});
-
-function makeDirectorID(givenId) {
-  let directorID = '';
-  if (givenId.length > 1) {
-    directorID = `D${givenId}`;
-  } else {
-    directorID = `D0${givenId}`;
-  }
-  return directorID;
-}
-
-let directorWithIds = {};
-let directorCount  = 1;
-listOfDirector.forEach((director) => {
-  const directorID = makeDirectorID(directorCount.toString());
-  directorWithIds[director] = directorID;
-  directorCount += 1;
 });
 
 
@@ -49,7 +40,7 @@ movieData.forEach((movie) => {
 
   fieldsMovie.forEach((field) => {
     if (field === 'Director') {
-      const directorId = directorWithIds[movie[field]];
+      const directorId = listOfDirector.indexOf(movie[field]) + 1;
       singleMovie.push(directorId);
     } else {
       singleMovie.push(movie[field]);
@@ -58,23 +49,34 @@ movieData.forEach((movie) => {
   movieArray.push(singleMovie);
 });
 
-console.log(movieArray);
-const directorArray = Object.entries(directorWithIds);
+const directorArray = [];
 
-console.log(directorArray);
+listOfDirector.forEach((director) => {
+  const singleDirector = [];
+  singleDirector.push(director);
+  directorArray.push(singleDirector);
+});
 
 connection.connect((err) => {
   if (err) {
     throw err;
   } else {
     console.log('Connected!');
-
-    connection.query(insertQueryDirector, [directorArray], (err, result, fields) => {
+    console.log('Creating tables....');
+    connection.query(createDirectorQuery, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    });
+    connection.query(createMovieQuery, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    });
+    connection.query(insertQueryDirector, [directorArray], (err, result) => {
       if (err) throw err;
       console.log(result);
     });
 
-    connection.query(insertQueryMovie, [movieArray], (err, result, fields) => {
+    connection.query(insertQueryMovie, [movieArray], (err, result) => {
       if (err) throw err;
       console.log(result);
     });
