3f71603b51e6f493b7db15f948c15fb8214f8d1c
learning-dev <ayazm.140895@gmail.com>
2019-12-02 13:10:56 +0530

    add corner cases for director table as well

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.put('/api/movies/:id', (req, res) => {
     const listOfFields = Object.keys(fieldsToUpdate);
     // if given fields are incorrect then show err or else show a success message.
     if (falseFields) {
-      msg = `Error: field '${listOfFields[failedIndex]}' is incorrect. Please `;
+      msg = `Error: field '${listOfFields[failedIndex]}' is incorrect. Please check the field name again.`;
       statusCode = 400;
     } else {
       msg = `All the fields i.e. ${listOfFields.join(', ')} are updated successfully`;
@@ -117,7 +117,14 @@ app.put('/api/movies/:id', (req, res) => {
 app.get('/api/directors', (req, res) => {
   const promiseObject = director.getAllDirector();
   promiseObject.then((result) => {
-    res.status(200).send(result);
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      throw (new Error('No data sent from table.'));
+    }
+  }).catch((err) => {
+    console.log(err);
+    res.status(404).send({ data: { error: "Internal error! can't retrieve Directors." } });
   });
 });
 
@@ -125,8 +132,16 @@ app.get('/api/directors/:id', (req, res) => {
   console.log(req.params.id);
   const promiseObject = director.getDirectorByID(req.params.id);
   promiseObject.then((result) => {
-
-    res.status(200).send(result);
+    console.log(result);
+    if (result.data.length > 0) {
+      res.status(200).send(result);
+    } else {
+      throw (new Error('Not found'));
+    }
+  }).catch((error) => {
+    console.log(error);
+    const errorMessage = { data: { errorMessage: `Resource with id ${req.params.id} doesn't exist` } };
+    res.status(404).send(errorMessage);
   });
 });
 
@@ -135,7 +150,17 @@ app.post('/api/directors/', (req, res) => {
   console.log(directorToAdd);
   const promiseObject = director.addDirector(directorToAdd);
   promiseObject.then((result) => {
-    res.status(201).send(result);
+    let msg;
+    let statusCode;
+    if (result && result.data.affectedRows > 0) {
+      msg = `Resource have been created with id ${result.data.insertId}`;
+      statusCode = 201;
+    } else {
+      msg = 'Error: can\'t resource. Make sure you check the fields and user previleges again or the resource already exists.';
+      statusCode = 400;
+    }
+    const resultJson = { data: { message: msg } };
+    res.status(statusCode).send(resultJson);
   }).catch((error) => {
     console.log(error);
   });
@@ -143,7 +168,19 @@ app.post('/api/directors/', (req, res) => {
 
 app.delete('/api/directors/:id', (req, res) => {
   const promiseObject = director.deleteDirector(req.params.id);
-  promiseObject.then((result) => res.status(200).send(result));
+  promiseObject.then((result) => {
+    let msg = '';
+    let statusCode;
+    if (result.data.affectedRows > 0) {
+      msg = `Resource with id ${req.params.id} is deleted sucessfully.`;
+      statusCode = 200;
+    } else {
+      msg = `Error: Can't delete the resource with id ${req.params.id}. Either is moved or doesn't exist.`;
+      statusCode = 404;
+    }
+    const resultJson = { data: { message: msg } };
+    res.status(statusCode).send(resultJson);
+  });
 });
 
 
@@ -152,12 +189,25 @@ app.put('/api/directors/:id', (req, res) => {
   const directorId = req.params.id;
   console.log(fieldsToUpdate);
   console.log(directorId);
+  let msg;
+  let statusCode;
   const promiseObject = director.updateDirector(directorId, fieldsToUpdate);
   promiseObject.then((result) => {
-    res.status(200).send(result);
+    if (result !== undefined && result.data.affectedRows === 0) {
+      msg = `Error: Item with id ${req.params.id} doesn't exitst`;
+      res.status(404).send({ data: { message: msg } });
+    }
+    const field = Object.keys(fieldsToUpdate)[0];
+    if (result === undefined) {
+      msg = `Error: field '${field}' is incorrect. Please check the field name again`;
+      statusCode = 400;
+    } else {
+      msg = `All the fields i.e. ${field} are updated successfully`;
+      statusCode = 200;
+    }
   }).catch((error) => {
     console.log(error);
-  });
+  }).finally(() => res.status(statusCode).send({ data: { message: msg } }));
 });
 
 const port = process.env.PORT || 3000;
